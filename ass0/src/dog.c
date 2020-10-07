0626ba140d2f33ba0db2319d0079e68f5b661a41
Jeffrey Zhang <37458680+chipmunkboi@users.noreply.github.com>
2020-10-07 01:40:52 -0700

    Figured out how to use system level functions(kinda)
    cat and dog for 2 txt files work except 2 ascii chars between

--- ass0/src/dog.c
+++ ass0/src/dog.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(){
-    
+int main(int argc, char *argv[]){
+    char buff[100];
+    char buff2[100];
+
+    int fone = open("abc.txt", O_RDONLY);
+    int ftwo = open("def.txt", O_RDONLY);
+
+    int rone = read(fone, buff, sizeof(buff));
+    int rtwo = read(ftwo, buff2, sizeof(buff2));
+
+    printf("%d", rone);
+
+    close(fone);
+    close(ftwo);
+
+    fprintf(stdout, "%s\n", buff);
+    fprintf(stdout, "%s", buff2);
 }
