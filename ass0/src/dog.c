6c32955c399bc46f0126e58c7c1538735d8991b4
Jeffrey Zhang <37458680+chipmunkboi@users.noreply.github.com>
2020-10-08 17:37:26 -0700

    finished except not working on jeffs:(

--- ass0/src/dog.c
+++ ass0/src/dog.c
@@ -4,20 +4,51 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <err.h>
+
+// reads from stdin then writes to stdout
+void userinput(char argzero[]){
+    char buf[1];
+    while((read(STDIN_FILENO, buf, 1)) != 0){ 
+        int write_check = write(1, buf, 1);
+        if (write_check == -1){
+            warn("%s", argzero);
+        }
+    }
+}
 
 int main(int argc, char *argv[]){
-    char buff[100]; // declare a 100 byte buffer to be used per file
+    // declare a 100 byte buffer to be used per file
+    char buff[100]; 
 
-    if(argc==1){ //
-        userinput();
+    // if only arg is "dog"
+    if (argc == 1){ 
+        userinput(argv[0]);
     }
 
-    for(int i=1; i<argc; i++){ // loops through argc files and prints each file
-        memset(buff, 0, sizeof(buff)); //clears buffer before reading new file
-        // check if file is valid
+    // loops through argc files and prints each file
+    for (int i=1; i<argc; i++){ 
+
+        // clears buffer before reading new file
+        memset(buff, 0, sizeof(buff)); 
         
+        // check if "-" is present for user input
+        printf("%s", argv[i]);
+        if (strcmp(argv[i], "-") == 0){
+            userinput(argv[i]);
+            continue;
+        }
 
         int fone = open(argv[i], O_RDONLY);
+        
+        // check if file is valid
+        if (fone == -1){
+            // file is not valid else continue
+            warn("%s", argv[i]);
+            close(fone);
+            continue;
+        }
         int rone = read(fone, buff, sizeof(buff));
         close(fone);
 
@@ -26,15 +57,3 @@ int main(int argc, char *argv[]){
     }
 }
 
-int userinput(){
-    char buf[1];
-    
-    while((read(STDIN_FILENO, buf, 1)) != 0){ //0 = EOF
-        int write_check = write(1, buf, 1);
-        if(write_check==-1){
-            warn("%s", argv[0]);
-            error_flag = 1;
-        }
-    }
-    return error_flag;
-}
