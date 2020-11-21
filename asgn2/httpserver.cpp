fca629cc30da1f0688a352689deca46df3bc129b
Jeffrey Zhang <37458680+chipmunkboi@users.noreply.github.com>
2020-11-20 22:35:22 -0800

    Update httpserver.cpp

--- asgn2/httpserver.cpp
+++ asgn2/httpserver.cpp
@@ -208,15 +208,6 @@ bool compareFiles(int file1, int file2){
 }
 
 void get_request (int comm_fd, struct httpObject* request, char* buf, bool rflag){
-
-    // printf("in GET\n");
-    // fflush(stdout);
-void get_request (int comm_fd, struct httpObject* request, char* buf){
-    // printf("in GET\n");
-    // fflush(stdout);
-void get_request (int comm_fd, struct httpObject* request, char* buf){
-    // printf("in GET\n");
-    // fflush(stdout);
     memset(buf, 0, SIZE);
     int file = open(request->filename, O_RDONLY);
 
