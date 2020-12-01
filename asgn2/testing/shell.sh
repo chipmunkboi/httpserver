a4812ba5b9c514f1c76b2b181eaf4ac3f502ddc1
Jeffrey Zhang <37458680+chipmunkboi@users.noreply.github.com>
2020-11-30 18:52:58 -0800

    Update shell.sh

--- asgn2/testing/shell.sh
+++ asgn2/testing/shell.sh
@@ -1,4 +1,4 @@
-# Multithreading testing with/without -r
+# Multithreading testing with and without redundancy
 
 curl -T cmd1.output http://localhost:8080/aaaaaaaaaa -v &
 curl -T cmd2.output http://localhost:8080/aaaaaaaaaa -v &
@@ -18,23 +18,22 @@ curl http://localhost:8080/cccccccccc -v &
 curl http://localhost:8080/dddddddddd -v & 
 
 
-# dd if=/dev/urandom of=d24 count=16 bs=1024 status=none
-# printf """PUT /13atestNoL HTTP/1.1\r\n\r\n""" \
-# | cat - d24 > nc-test24.cmds
-# ncat -d 2 localhost 8080 < nc-test24.cmds
+####################################################################
+# General error testing
+####################################################################
 
 # GET REQUESTS
-# curl http://localhost:8080/aaaaaaaaaa -v                    # valid 
-# curl http://localhost:8080/zzzzzzzzzz -v                    # nonexistent file
-# curl http://localhost:8080/aaaaaaaaa! -v                    # invalid name: non alphanumeric
-# curl http://localhost:8080/aaaaaaaaa -v                     # invalid name: <10 ASCII
-# curl http://localhost:8080/aaaaaaaaaaa -v                   # invalid name: >10 ASCII
+curl http://localhost:8080/aaaaaaaaaa -v                    # valid 
+curl http://localhost:8080/zzzzzzzzzz -v                    # nonexistent file
+curl http://localhost:8080/aaaaaaaaa! -v                    # invalid name: non alphanumeric
+curl http://localhost:8080/aaaaaaaaa -v                     # invalid name: <10 ASCII
+curl http://localhost:8080/aaaaaaaaaaa -v                   # invalid name: >10 ASCII
 
 # #PUT REQUESTS
-# curl -T cmd1.output http://localhost:8080/aaaaaaaaaa -v     # valid
-# curl -T cmd1.output http://localhost:8080/aaaaaaaaa! -v     # invalid name: non alphanumeric
-# curl -T cmd1.output http://localhost:8080/aaaaaaaaa -v      # invalid name: <10 ASCII
-# curl -T cmd1.output http://localhost:8080/aaaaaaaaaaa -v    # invalid name: >10 ASCII
+curl -T cmd1.output http://localhost:8080/aaaaaaaaaa -v     # valid
+curl -T cmd1.output http://localhost:8080/aaaaaaaaa! -v     # invalid name: non alphanumeric
+curl -T cmd1.output http://localhost:8080/aaaaaaaaa -v      # invalid name: <10 ASCII
+curl -T cmd1.output http://localhost:8080/aaaaaaaaaaa -v    # invalid name: >10 ASCII
 
 
 
